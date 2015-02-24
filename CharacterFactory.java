7d2fcacd0dcc5e25c03dd6af5080730e6aeba466
kevinMurr <kmurray@eagle.ewu.edu>
2015-02-24 11:44:59 -0800

    changed how factory works

--- CharacterFactory.java
+++ CharacterFactory.java
@@ -1,20 +1,5 @@
-public class CharacterFactory
+public interface CharacterFactory
 {
-	public DungeonCharacter orderEnemy(String name)
-	{
-		StatsFactory statFact = StatsFactory.getStatFact();
-		
-		if(name.equals("Blob"))
-			return new Blob(statFact.getStats("Blob"));
-		else
-		if(name.equals("Wizard"))
-			return new Wizard(statFact.getStats("Wizard"));
-		else
-		if(name.equals("Paladin"))
-			return new Paladin(statFact.getStats("Paladin"));
-		
-			
-		return null;
-	}
+	public DungeonCharacter order(String name);
 
 }
